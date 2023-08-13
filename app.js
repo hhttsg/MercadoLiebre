c1d1f9821d73d58c53ad302243068db813ef553c
Santos <giron44706@gmail.com>
2023-08-13 18:01:27 -0300

    cambios en pakage.json

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ const pathPublic= path.resolve(__dirname,'./public');
 app.use(express.static(pathPublic));
 
 const port = process.env.port || 3001;
-app.listen(port, ()=>console.log('Servidor corriendoen el puerto ${port}'));
+app.listen(port, ()=>console.log('Servidor corriendoen el puerto ${port}'))
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/home.html'));
-})
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));})
 app.get('/register',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/register.html'));
-})
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));})
 app.get('/login',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/login.html'));
-})
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));})
